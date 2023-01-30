fee3f75ab4643bbcbbb84cc59168a4fdc76784f6
Gamingwithchuqmuq <95252012+CuriousCoder1232@users.noreply.github.com>
2023-01-30 17:23:46 -0500

    Delete index.js

--- index.js
+++
@@ -1,4 +0,0 @@
-(async() => {
-  await import('./index.mjs');
-})();
-//Hello! If you are reading this, that means that you forked my Replit and now have a working proxy. Please let me know if you liked the proxy by leaving a review in the comments. Also make sure to follow me and check out my other proxies. Thank you, and have a wonderful day!
